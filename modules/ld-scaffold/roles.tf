1ace82489daebf0b61cb57fe1d6fc622903ea23d
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-07-31 11:09:10 +0100

    Fixing typo (#23)

--- modules/ld-scaffold/roles.tf
+++ modules/ld-scaffold/roles.tf
@@ -27,7 +27,7 @@ resource "launchdarkly_custom_role" "roles" {
   }
 
   policy {
-    effect    = "Deny"
+    effect    = "deny"
     resources = ["proj/${each.value.name}:env/*"]
     actions   = ["createEnvironment", "deleteEnvironment"]
   }
