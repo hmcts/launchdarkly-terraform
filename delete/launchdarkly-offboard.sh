52d4e8af93eeb54b0d3d80ca793922a75a00fc0f
mokainos <72137340+mokainos@users.noreply.github.com>
2021-03-10 19:35:58 +0000

    Update launchdarkly-offboard.sh

--- delete/launchdarkly-offboard.sh
+++ delete/launchdarkly-offboard.sh
@@ -7,11 +7,10 @@ IFS=,
 
 
 function deleteuser() {
-        echo $1
-      curl -v --request DELETE \
+        curl --request DELETE \
           --url "https://app.launchdarkly.com/api/v2/members/$1" \
           --header "Authorization: $TOKEN" \
-          --header "LD-API-Version: beta"
+          --header 'LD-API-Version: beta'
 
 }
 curl  -s --request GET   --url 'https://app.launchdarkly.com/api/v2/members?limit=200&offset=0'   --header "Authorization: $TOKEN"   --header 'LD-API-Version: beta' | jq -r '.items | map({_id, email, _lastSeen})| (first | keys_unsorted) as $keys | map([to_entries[] | .value]) as $rows | $keys,$rows[] | @csv' | tr -d \" | grep -v _lastSeen| while read id email lastseen
